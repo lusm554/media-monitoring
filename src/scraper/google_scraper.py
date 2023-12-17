33437cebb293920e8f47d957d12f323d1f409ac8
lusm <loveyousomuch554@gmail.com>
2023-12-17 13:30:01 +0300

    go scrapper

--- src/scraper/google_scraper.py
+++ src/scraper/google_scraper.py
@@ -3,70 +3,73 @@ from article import Article
 from pprint import pprint
 import requests
 import datetime
+import time
 
-def convert_publish_to_datetime(publish_dt):
-  time_unit, unit_type, _ = publish_dt.split(maxsplit=2) 
-  if 'час' in unit_type:
-    publish_time = datetime.datetime.now() - datetime.timedelta(hours=int(time_unit))
-  elif 'мин' in unit_type:
-    publish_time = datetime.datetime.now() - datetime.timedelta(minutes=int(time_unit))
-  else:
-    publish_time = datetime.datetime.now()
-  return publish_time
- 
-def parse_page(html):
-  only_tags_with_id_search = SoupStrainer(id='search')
-  soup = BeautifulSoup(html, 'lxml', parse_only=only_tags_with_id_search)
-  search_links = soup.find_all('a')
-  result = []
-  if len(search_links) == 0:
+class GoogleScraper:
+  def __init__(self):
+    pass
+
+  def convert_publish_to_datetime(self, publish_dt):
+    time_unit, unit_type, _ = publish_dt.split(maxsplit=2) 
+    if 'час' in unit_type:
+      publish_time = datetime.datetime.now() - datetime.timedelta(hours=int(time_unit))
+    elif 'мин' in unit_type:
+      publish_time = datetime.datetime.now() - datetime.timedelta(minutes=int(time_unit))
+    else:
+      publish_time = datetime.datetime.now()
+    return publish_time
+   
+  def parse_page(self, html):
+    only_tags_with_id_search = SoupStrainer(id='search')
+    soup = BeautifulSoup(html, 'lxml', parse_only=only_tags_with_id_search)
+    search_links = soup.find_all('a')
+    result = []
+    if len(search_links) == 0:
+      return result
+    for element_a in search_links:
+      link = element_a.get('href')
+      title = element_a.find(attrs={'role': 'heading'}).string
+      source_name = element_a.find('span').string
+      publish_time = element_a.find_all('span')[-1].string
+      article = Article(
+        title=title,
+        url=link,
+        publish_time=self.convert_publish_to_datetime(publish_time),
+        publisher_name=source_name,
+      )
+      result.append(article)
     return result
-  for element_a in search_links:
-    link = element_a.get('href')
-    title = element_a.find(attrs={'role': 'heading'}).string
-    source_name = element_a.find('span').string
-    publish_time = element_a.find_all('span')[-1].string
-    article = Article(
-      title=title,
-      url=link,
-      publish_time=convert_publish_to_datetime(publish_time),
-      publisher_name=source_name,
-    )
-    result.append(article)
-  return result
 
-headers = {
-    'authority': 'www.google.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-    'sec-ch-ua-arch': '"arm"',
-    'sec-ch-ua-bitness': '"64"',
-    'sec-ch-ua-full-version': '"116.0.5843.0"',
-    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5843.0"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-model': '""',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-ch-ua-platform-version': '"13.4.0"',
-    'sec-ch-ua-wow64': '?0',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-}
-
-if __name__ == '__main__':
-  for i in range(0, 500, 10):
+  def fetch_page(self, page_num):
+    headers = {
+        'authority': 'www.google.com',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+        'sec-ch-ua-arch': '"arm"',
+        'sec-ch-ua-bitness': '"64"',
+        'sec-ch-ua-full-version': '"116.0.5843.0"',
+        'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5843.0"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-model': '""',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-ch-ua-platform-version': '"13.4.0"',
+        'sec-ch-ua-wow64': '?0',
+        'ec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+    }
     params = {
       'q': 'ЦФА',
       'tbm': 'nws',
       'source': 'lnt',
       'tbs': 'lr:lang_1ru,qdr:d',
       'lr': 'lang_ru',
-      'start': i
+      'start': page_num 
     }
     response = requests.get(
         f'https://www.google.com/search',
@@ -75,11 +78,21 @@ if __name__ == '__main__':
     )
     print(response.url)
     html = response.text
-    links = parse_page(html)
-    if links == []:
-      break
+    return html
+  
+  def fetch_articles(self):
+    articles = []
+    for page_num in range(0, 500, 10):
+      html = self.fetch_page(page_num)
+      _arts = self.parse_page(html)
+      if len(_arts) == 0:
+        return articles
+      articles.extend(_arts)
+      time.sleep(.5)
+    return articles
 
-    for l in links:
-      print(l.title, l.publish_time)
-    print();print()
-    break
+if __name__ == '__main__':
+  scrp = GoogleScraper()
+  arts = scrp.fetch_articles()
+  for i in arts:
+    print(i) 
--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,4 +1,5 @@
 from rss_scraper import rss_scraper
+from google_scraper import GoogleScraper
 from pprint import pprint
 
 class Scraper:
@@ -7,11 +8,18 @@ class Scraper:
     self.from_google = from_google 
 
   def get_articles_from_rss(self):
-    rss_articles = rss_scraper().fetch_last_news()
+    rss_articles = self.from_rss().fetch_last_news()
     return rss_articles
     
-
+  def get_articles_from_google(self):
+    go_articles = self.from_google().fetch_articles()
+    return go_articles
 
 if __name__ == '__main__':
-  scrp = Scraper(from_rss=rss_scraper, from_google=None)
-  scrp.get_articles_from_rss()
+  scrp = Scraper(from_rss=rss_scraper, from_google=GoogleScraper)
+  _1 = scrp.get_articles_from_rss()
+  _2 = scrp.get_articles_from_google()
+  pprint(_1)
+  print('\n'*3)
+  pprint(_2)
+
