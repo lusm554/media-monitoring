1a6f884cfd57872780c24055174d04b83378a064
lusm <loveyousomuch554@gmail.com>
2023-12-27 14:34:29 +0300

    ffffffff

--- src/main.py
+++ src/main.py
@@ -142,9 +142,9 @@ async def error_handler(update, context):
   tb_string = "".join(tb_list)
   update_str = update.to_dict() if isinstance(update, Update) else str(update)
   message = (
-    "An exception was raised while handling an update\n"
+    f"An exception was raised while handling an update\n"
     f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
-    "</pre>\n\n"
+    f"</pre>\n\n"
     f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
     f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
     f"<pre>{html.escape(tb_string)}</pre>"
