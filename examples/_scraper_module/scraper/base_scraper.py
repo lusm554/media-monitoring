9ad92cc41dd541d86ab86fae1b5a666c47b7ae4f
lusm <loveyousomuch554@gmail.com>
2024-04-13 13:54:25 +0300

    doc string for Periods BaseScraper NewsBaseScraper

--- examples/_scraper_module/scraper/base_scraper.py
+++ examples/_scraper_module/scraper/base_scraper.py
@@ -1,9 +1,16 @@
 class Periods:
+  '''
+  Класс периодов для фильтрации новостей и выпусков.
+  '''
   LAST_24_HOURS = 1
   LAST_WEEK = 2
   ALL_AVAILABLE_TIME = 3
 
 class BaseScraper:
+  '''
+  Базовый класс для всех скреперов.
+  Содержит общие методы для всех скреперов. 
+  '''
   def filter_by_period(self):
     # check for instance of Periods or datetime
     pass
@@ -12,6 +19,10 @@ class BaseScraper:
     pass
 
 class NewsBaseScraper(BaseScraper):
+  '''
+  Базовый класс для скреперов новостей.
+  Содержит общие методы для скреперов новостей. 
+  '''
   def __init__(self):
     self.cfa_news_url_blacklist = (
       'echomsk.spb.ru',
@@ -21,9 +32,3 @@ class NewsBaseScraper(BaseScraper):
   
   def filter_news_by_blacklist(self):
     pass
-
-
-if __name__ == '__main__':
-  # print(Periods.LAST_24_HOURS)
-  # print(Periods.LAST_WEEK)
-  # print(Periods.ALL_AVAILABLE_TIME)
