073fb29da7be3f85b8c9f34b747e1a310d96503b
lusm <loveyousomuch554@gmail.com>
2024-04-13 12:56:20 +0300

    rename scraper old files

--- examples/_scraper_module/scraper/__init__.py
+++ examples/_scraper_module/scraper/__init__.py
@@ -1,6 +1,6 @@
-from .scraper import get_scraper_instance
-from .rss_scraper import RSS
-from .google_scraper import GoogleScraper
-from .dzen_scraper import DzenScraper
-from .article import WrappedArticle, Article
-from .scraper import Scraper
+# from .scraper import get_scraper_instance
+# from .rss_scraper import RSS
+# from .google_scraper import GoogleScraper
+# from .dzen_scraper import DzenScraper
+# from .article import WrappedArticle, Article
+# from .scraper import Scraper
