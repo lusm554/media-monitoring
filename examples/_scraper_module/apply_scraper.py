2fb4a1f81519e0a8ba2d26b0d73caa319a7faff5
lusm <loveyousomuch554@gmail.com>
2024-04-13 11:40:47 +0300

    plan notes

--- examples/_scraper_module/apply_scraper.py
+++ examples/_scraper_module/apply_scraper.py
@@ -1,3 +1,24 @@
-from scraper import scraper
+import logging
+from scraper import Scraper
 
+logging.basicConfig(
+  format='[%(asctime)s] %(levelname)s [%(name)s] %(message)s',
+  datefmt='%Y-%m-%d %H:%M:%S',
+  level=logging.INFO,
+)
+
+Scraper:
+  News:
+    - rss
+    - google
+    - dzen
+  Emits:
+    - cfa_ru
+
+Module public classes:
+  CfaNewsScraper
+  CfaGoogleNewsScraper
+  CfaDzenNewsScraper
+  CfaRssNewsScraper
+  CfaReleasesScraper
 
--- examples/_scraper_module/scraper/__init__.py
+++ examples/_scraper_module/scraper/__init__.py
@@ -3,3 +3,4 @@ from .rss_scraper import RSS
 from .google_scraper import GoogleScraper
 from .dzen_scraper import DzenScraper
 from .article import WrappedArticle, Article
+from .scraper import Scraper
