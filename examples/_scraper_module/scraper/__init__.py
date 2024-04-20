38a7ba6aca298c9dd7265fad3fa4c71a502d09fd
lusm <loveyousomuch554@gmail.com>
2024-04-20 19:21:26 +0300

    add cfaru scraper to module imports

--- examples/_scraper_module/scraper/__init__.py
+++ examples/_scraper_module/scraper/__init__.py
@@ -10,3 +10,4 @@ from .dzen_news_scraper import CfaDzenNewsScraper
 from .rss_news_scraper import CfaRssNewsScraper
 from .google_news_scraper import CfaGoogleNewsScraper
 from .all_news_scraper import CfaAllNewsScraper
+from .cfaru_releases_scraper import CfaReleasesScraper
