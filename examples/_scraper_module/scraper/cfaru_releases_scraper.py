16f12b492cb0c40189b600189182580a8643fc31
lusm <loveyousomuch554@gmail.com>
2024-04-20 19:42:17 +0300

    add page fetcher for cfa ru

--- examples/_scraper_module/scraper/cfaru_releases_scraper.py
+++ examples/_scraper_module/scraper/cfaru_releases_scraper.py
@@ -1,11 +1,23 @@
 from .base_scraper import BaseScraper
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
 import logging
 
 logger = logging.getLogger(__name__)
 
 class CfaReleasesScraper(BaseScraper):
   def page_fetcher(self):
-    pass
+    response = requests.get(
+      f'https://цфа.рф/cfa-vypusk.html',
+      timeout=2, # seconds
+    )
+    response.encoding = 'utf-8'
+    logger.info(f'Cfaru fetched url {response.url}')
+    logger.debug(f'Fetched status {response.status_code}')
+    logger.info(f'Cfaru fetched in {response.elapsed.total_seconds()} seconds')
+    assert response.status_code == 200
+    html = response.text
+    return html
   
   def page_parser(self):
     pass
