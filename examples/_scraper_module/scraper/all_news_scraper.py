73fcbdb329de7eeef075d97c0944323b22187fc5
lusm <loveyousomuch554@gmail.com>
2024-04-20 12:29:10 +0300

    add scrapers list in init all news class

--- examples/_scraper_module/scraper/all_news_scraper.py
+++ examples/_scraper_module/scraper/all_news_scraper.py
@@ -1,4 +1,13 @@
 from .base_scraper import NewsBaseScraper
+from .dzen_news_scraper import CfaDzenNewsScraper
+from .rss_news_scraper import CfaRssNewsScraper
+from .google_news_scraper import CfaGoogleNewsScraper
 
 class CfaAllNewsScraper(NewsBaseScraper):
-  pass
+  def __init__(self):
+    super().__init__()
+    self.NEWS_SCRAPERS = [
+      CfaDzenNewsScraper,
+      CfaRssNewsScraper,
+      CfaGoogleNewsScraper,
+    ]
