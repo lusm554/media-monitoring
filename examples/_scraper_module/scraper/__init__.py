49e320c0ed5bade4b1b497e25fab8b8e372a3361
lusm <loveyousomuch554@gmail.com>
2024-04-20 11:44:24 +0300

    add all news class to scraper module imports

--- examples/_scraper_module/scraper/__init__.py
+++ examples/_scraper_module/scraper/__init__.py
@@ -8,4 +8,5 @@
 from .base_scraper import Periods
 from .dzen_news_scraper import CfaDzenNewsScraper
 from .rss_news_scraper import CfaRssNewsScraper
-from .google_news_scraper import CfaGoogleNewsScraper 
+from .google_news_scraper import CfaGoogleNewsScraper
+from .all_news_scraper import CfaAllNewsScraper
