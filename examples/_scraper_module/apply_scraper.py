c3e431ed323a958ed368ac4002cc5b276b2d032f
lusm <loveyousomuch554@gmail.com>
2024-04-21 21:27:06 +0300

    example how to apply

--- examples/_scraper_module/apply_scraper.py
+++ examples/_scraper_module/apply_scraper.py
@@ -19,44 +19,44 @@ def how_to_apply_cfaru_releases_scraper():
   # cfa_releases = CfaReleasesScraper().fetch_and_parse(period=Periods.LAST_24_HOURS)
   cfa_releases = CfaReleasesScraper().fetch_and_parse(period=Periods.LAST_WEEK)
   plt = None
-  for release in sorted(cfa_releases, key=lambda x:x.platform_name):
-    if plt != release.platform_name:
-      plt = release.platform_name
-      print(plt)
-      print('\t', release.release_time, release.title)
-    else:
-      print('\t', release.release_time, release.title)
+  # for release in sorted(cfa_releases, key=lambda x:x.platform_name):
+  #   if plt != release.platform_name:
+  #     plt = release.platform_name
+  #     print(plt)
+  #     print('\t', release.release_time, release.title)
+  #   else:
+  #     print('\t', release.release_time, release.title)
 how_to_apply_cfaru_releases_scraper()
 
 def how_to_apply_dzen_news_scraper():
   from scraper import CfaDzenNewsScraper, Periods
   dzen_news = CfaDzenNewsScraper().fetch_and_parse(period=Periods.LAST_24_HOURS)
   # dzen_news = CfaDzenNewsScraper().fetch_and_parse(period=Periods.LAST_WEEK)
-  pprint(dzen_news)
+  # pprint(dzen_news)
 # how_to_apply_dzen_news_scraper()
 
 def how_to_apply_rss_news_scraper():
   from scraper import CfaRssNewsScraper, Periods
-  rss_news = CfaRssNewsScraper().fetch_and_parse(period=Periods.LAST_24_HOURS)
-  # rss_news = CfaRssNewsScraper().fetch_and_parse(period=Periods.LAST_WEEK)
-  pprint(rss_news)
+  # rss_news = CfaRssNewsScraper().fetch_and_parse(period=Periods.LAST_24_HOURS)
+  rss_news = CfaRssNewsScraper().fetch_and_parse(period=Periods.LAST_WEEK)
+  # pprint(rss_news)
 # how_to_apply_rss_news_scraper()
 
 def how_to_apply_google_news_scraper():
   from scraper import CfaGoogleNewsScraper, Periods
   google_news = CfaGoogleNewsScraper().fetch_and_parse(period=Periods.LAST_24_HOURS)
   # google_news = CfaGoogleNewsScraper().fetch_and_parse(period=Periods.LAST_WEEK)
-  print(f'{len(google_news)=}')
-  pprint(google_news)
+  # print(f'{len(google_news)=}')
+  # pprint(google_news)
 # how_to_apply_google_news_scraper()
 
 def how_to_apply_all_news_scraper():
   from scraper import CfaAllNewsScraper, Periods
-  all_news = CfaAllNewsScraper().fetch_and_parse(period=Periods.LAST_24_HOURS)
-  # all_news = CfaAllNewsScraper().fetch_and_parse(period=Periods.LAST_WEEK)
-  print(f'{len(all_news)=}')
-  print(f'{len(set(all_news))=}')
-  pprint(all_news[:5])
+  # all_news = CfaAllNewsScraper().fetch_and_parse(period=Periods.LAST_24_HOURS)
+  all_news = CfaAllNewsScraper().fetch_and_parse(period=Periods.LAST_WEEK)
+  # print(f'{len(all_news)=}')
+  # print(f'{len(set(all_news))=}')
+  # pprint(all_news[:5])
 # how_to_apply_all_news_scraper()
 
 '''
