dffb1f08a9cdfdb08f0ca521ee2c6d4d6d758536
lusm <loveyousomuch554@gmail.com>
2024-04-21 11:49:34 +0300

    release class split to own module-file

--- examples/_scraper_module/scraper/cfaru_releases_scraper.py
+++ examples/_scraper_module/scraper/cfaru_releases_scraper.py
@@ -1,4 +1,5 @@
 from .base_scraper import BaseScraper
+from .release import Release
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import re
@@ -7,31 +8,6 @@ import urllib
 import logging
 
 logger = logging.getLogger(__name__)
-
-class Release:
-  __slots__ = ('platform_name', 'url', 'release_time', 'title')
-  def __init__(self, platform_name, url, release_time, title):
-    self.platform_name = platform_name
-    self.url = url
-    self.release_time = release_time
-    self.title = title
-
-  def __eq__(self, other):
-    if not isinstance(other, self.__class__):
-      raise ValueError(f'Cannot compare instance {other!r} of different class')
-    return self.url == other.url
-
-  def __ne__(self, other):
-    if not isinstance(other, self.__class__):
-      raise ValueError(f'Cannot compare instance {other!r} of different class')
-    return self.url != other.url
-
-  def __hash__(self):
-    return hash(self.url)
-
-  def __repr__(self):
-    items = [ (attr, getattr(self, attr)) for attr in self.__slots__ ]
-    return f'{self.__class__.__name__}(\n{",\n".join(f"{k}={v!r}"  for k,v in items)}\n)'
 
 class CfaReleasesScraper(BaseScraper):
   def page_fetcher(self):
