319d0e321743c78c0ace3fe5015d9b3d489683d0
lusm <loveyousomuch554@gmail.com>
2024-04-21 17:28:48 +0300

    add doc string for all news scraper

--- examples/_scraper_module/scraper/all_news_scraper.py
+++ examples/_scraper_module/scraper/all_news_scraper.py
@@ -8,6 +8,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 class CfaAllNewsScraper(NewsBaseScraper):
+  '''
+  Объединяет в себе все парсеры новостей для удобства парсинга всех новостей за раз.
+  '''
   def __init__(self):
     super().__init__()
     self.NEWS_SCRAPERS = [
@@ -17,12 +20,19 @@ class CfaAllNewsScraper(NewsBaseScraper):
     ]
 
   def filter_by_blacklist(self, articles):
+    '''
+    Исключает новости, домены которых находятся в черном списке self.cfa_news_url_blacklist.
+    '''
     return [
       article for article in articles
       if not urlparse(article.url).netloc in self.cfa_news_url_blacklist
     ]
 
   def fetch_and_parse(self, period):
+    '''
+    Забирает новости по каждому парсеру, вызывая метод fetch_and_parse у каждого парсера.
+    Формирует конченый список уникальных новостей.
+    '''
     all_scrapers_articles = list()
     for scraper in self.NEWS_SCRAPERS:
       scraper_articles = scraper().fetch_and_parse(period=period)
