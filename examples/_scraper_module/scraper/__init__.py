967574d45bbddd21a7bf354dc7d2dffc428c9eb2
lusm <loveyousomuch554@gmail.com>
2024-04-21 15:01:42 +0300

    remove old module imports

--- examples/_scraper_module/scraper/__init__.py
+++ examples/_scraper_module/scraper/__init__.py
@@ -1,10 +1,3 @@
-# from .scraper import get_scraper_instance
-# from .rss_scraper import RSS
-# from .google_scraper import GoogleScraper
-# from .dzen_scraper import DzenScraper
-# from .article import WrappedArticle, Article
-# from .scraper import Scraper
-
 from .base_scraper import Periods
 from .dzen_news_scraper import CfaDzenNewsScraper
 from .rss_news_scraper import CfaRssNewsScraper
