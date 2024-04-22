6de9897c5584120467d832c8b81aa90cf7298b87
lusm <loveyousomuch554@gmail.com>
2024-04-22 15:19:54 +0300

    plan: start

---
+++ src/main.py
@@ -0,0 +1,28 @@
+'''
+Commands:
+  Standard:
+    - help
+    - start
+  Specific:
+    - last_news
+    - emits
+    - media_index
+    - media_blacklist
+    - set_news_schedule
+    - unset_news_schedule
+
+Functionality:
+  Main:
+    - cfa news
+    - cfa releases
+    - regular cfa news and releases
+  Side:
+    - show news blacklist
+    - show rss media index
+  
+Structure:
+  commands/
+  scraper/
+  main.py
+  ...other.py
+'''
