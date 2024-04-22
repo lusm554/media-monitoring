3842e519ae79c1946df3a16ff07c1a6e30f18586
lusm <loveyousomuch554@gmail.com>
2024-04-22 15:45:38 +0300

    add logging config to main

--- src/main.py
+++ src/main.py
@@ -31,3 +31,20 @@ Handlers:
   2. buttons
   3. Updates
 '''
+
+from timezone import time_zone_moscow
+import datetime
+import logging
+
+logging.Formatter.converter = lambda *args: datetime.datetime.now(tz=time_zone_moscow).timetuple()
+logging.basicConfig(
+  level=logging.INFO,
+  format='[%(asctime)s] %(levelname)s [%(name)s] %(message)s',
+  datefmt='%Y-%m-%d %H:%M:%S %Z',
+  handlers=[
+    logging.FileHandler(datetime.datetime.now().strftime('logs/log_%Y-%m-%d_%H-%M-%S.log')),
+    logging.StreamHandler(),
+  ],
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
