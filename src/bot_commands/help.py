a58b4c1798550d7472453a55ec41ce315a270c2e
lusm <loveyousomuch554@gmail.com>
2024-04-23 14:06:53 +0300

    add help message content for bot commands

--- src/bot_commands/help.py
+++ src/bot_commands/help.py
@@ -1,5 +1,8 @@
 async def help_(update, context):
-  help_msg = 'later'
+  help_msg = '\n'.join(
+    f'/{cmd.name} - {cmd.desc}'
+    for cmd in context.bot_data.get("commands", [])
+  )
   await context.bot.send_message(
     chat_id=update.effective_chat.id,
     text=help_msg
