998055e3f18e1a2b872722758303cff16f128e74
lusm <loveyousomuch554@gmail.com>
2024-04-28 20:12:22 +0300

    add function for cfa last news markup generation

--- src/bot_commands/cfa_last_news.py
+++ src/bot_commands/cfa_last_news.py
@@ -1,6 +1,14 @@
+import datetime
+from uuid import uuid4
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.constants import ParseMode
 import logging
 
 logger = logging.getLogger(__name__)
+
+RIGHT_ARROW_SYMBOL = chr(8594) # →
+LEFT_ARROW_SYMBOL = chr(8592) # ←
+CFA_LAST_NEWS_CALLBACK_ID = 'cfa_last_news'
 
 class Post:
   def __init__(self, post_id, post_articles, creation_time=None):
@@ -35,6 +43,30 @@ class Post:
   @property
   def current_stage_text(self):
     return f'{self._current_page+1}/{self._pages_size+1}'
+
+def get_cfa_last_news_post_markup(post):
+  msg = '\n\n'.join(
+    f'{n}. <a href="{article.url}"> {article.title} </a>\n'
+    f'<b>Источник:</b> {article.publisher_name}.\n'
+    f'<b>Опубликовано:</b> {article.publish_time}.\n'
+    f'<b>Взято из:</b> {article.scraper}.'
+    for n, article in enumerate(
+      post.current_page(),
+      start=post._current_page * post._post_count_on_page + 1
+    )
+  )
+  internal_post_id = post.post_id
+  callback_id = CFA_LAST_NEWS_CALLBACK_ID
+  pagination_keyboard = [
+    [
+      InlineKeyboardButton(LEFT_ARROW_SYMBOL, callback_data=f'{callback_id}_backward_{internal_post_id}'),
+      InlineKeyboardButton(post.current_stage_text, callback_data=f'{callback_id}_counter_{internal_post_id}'),
+      InlineKeyboardButton(RIGHT_ARROW_SYMBOL, callback_data=f'{callback_id}_forward_{internal_post_id}')
+    ],
+  ]
+  keyboard_markup = InlineKeyboardMarkup(pagination_keyboard)
+  return msg, keyboard_markup
+
 async def cfa_last_news(update, context):
   scraper = context.bot_data.get("scraper")
   articles = scraper.CfaAllNewsScraper().fetch_and_parse(scraper.Periods.LAST_24_HOURS)
