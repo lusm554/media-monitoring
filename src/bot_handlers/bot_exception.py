d89d4d93730c5171d3e645a73ee0fcc8af43dd19
lusm <loveyousomuch554@gmail.com>
2024-04-28 19:18:06 +0300

    less len in bot exception handlers

--- src/bot_handlers/bot_exception.py
+++ src/bot_handlers/bot_exception.py
@@ -10,14 +10,15 @@ async def error_handler(update, context):
   tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
   tb_string = "".join(tb_list)
   update_str = update.to_dict() if isinstance(update, Update) else str(update)
+  update_html = html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))
+  update_html = update_html[:2000]
   message = (
     f"An exception was raised while handling an update\n"
-    f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+    f"<pre>update = {update_html}"
     f"</pre>\n\n"
-    f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
-    f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
     f"<pre>{html.escape(tb_string)}</pre>"
   )
+  logger.info(f'Error message size {len(message)}')
   await context.bot.send_message(
     chat_id=os.environ.get('DEVELOPER_CHAT_ID'), text=message, parse_mode=ParseMode.HTML
   )
