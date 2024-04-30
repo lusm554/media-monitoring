659df48977f256bb8cc16134b64e6b7ce9bc5020
lusm <loveyousomuch554@gmail.com>
2024-04-30 15:45:54 +0300

    separate Post class to own file

--- src/bot_commands/cfa_last_news.py
+++ src/bot_commands/cfa_last_news.py
@@ -1,7 +1,7 @@
-import datetime
 from uuid import uuid4
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.constants import ParseMode
+from .post import Post
 import logging
 
 logger = logging.getLogger(__name__)
@@ -9,40 +9,6 @@ logger = logging.getLogger(__name__)
 RIGHT_ARROW_SYMBOL = chr(8594) # →
 LEFT_ARROW_SYMBOL = chr(8592) # ←
 CFA_LAST_NEWS_CALLBACK_ID = 'cfa_last_news'
-
-class Post:
-  def __init__(self, post_id, post_articles, creation_time=None):
-    self._post_count_on_page = 4
-    self._current_page = 0
-    self.post_id = str(post_id)
-    self.post_articles = post_articles
-    self.pages = [
-      self.post_articles[i:i+self._post_count_on_page]
-      for i in range(0, len(self.post_articles), self._post_count_on_page)
-    ]
-    self._pages_size = len(self.pages) - 1
-    self.creation_time = creation_time or datetime.datetime.now()
-
-  def current_page(self):
-    return self.pages[self._current_page]
-
-  def next_page(self):
-    if self._current_page == self._pages_size:
-      self._current_page = 0
-    else:
-      self._current_page += 1
-    return self.pages[self._current_page]
-
-  def previous_page(self):
-    if self._current_page == 0:
-      self._current_page = self._pages_size
-    else:
-      self._current_page -= 1
-    return self.pages[self._current_page]
-
-  @property
-  def current_stage_text(self):
-    return f'{self._current_page+1}/{self._pages_size+1}'
 
 def get_cfa_last_news_post_markup(post):
   msg = '\n\n'.join(
