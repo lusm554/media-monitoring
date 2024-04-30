9aac0b28ed2b93f6d4d59b6c4a31de3da04cbc4b
lusm <loveyousomuch554@gmail.com>
2024-04-30 18:48:17 +0300

    add command cfa releases for last 24 hours

--- src/bot_commands/__init__.py
+++ src/bot_commands/__init__.py
@@ -1,3 +1,4 @@
 from .start import start
 from .help import help_
 from .cfa_last_news import cfa_last_news, cfa_week_news, cfa_last_news_button_callback
+from .cfa_releases import cfa_last_releases, cfa_releases_button_callback
