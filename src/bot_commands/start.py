2a1ac87899625d2ced45fdf8ace9bb0c16ea4217
lusm <loveyousomuch554@gmail.com>
2024-05-01 17:32:00 +0300

    change start message

--- src/bot_commands/start.py
+++ src/bot_commands/start.py
@@ -1,5 +1,11 @@
 async def start(update, context):
+  msg = (
+    f'Привет!\n'
+    f'Этот бот собирает публикации СМИ и выпуски по ЦФА.\n'
+    f'Рассылка новостей /subscribe_news.\n'
+    f'Более подробно через команду /help.'
+  )
   await context.bot.send_message(
     chat_id=update.effective_chat.id,
-    text=f'test here later'
+    text=msg
   )
