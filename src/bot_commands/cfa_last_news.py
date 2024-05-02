8ecac0df9d8cccf465770a4e78ea7bd2403df035
lusm <loveyousomuch554@gmail.com>
2024-05-02 19:37:15 +0300

    add scraper mem cache class

--- src/bot_commands/cfa_last_news.py
+++ src/bot_commands/cfa_last_news.py
@@ -1,5 +1,6 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.constants import ParseMode
+import datetime
 from .post import Post
 import scraper
 import logging
@@ -37,6 +38,34 @@ def transform_update_context(func):
   async def wrapper(update, context):
     return await func(context=context, effective_chat_id=update.effective_chat.id)
   return wrapper
+
+class ScraperMemCache:
+  def __init__(self, items, cache_key, expire_delta=datetime.timedelta(minutes=15)):
+    self.items = items
+    self.cache_key = cache_key
+    self.creation_time = datetime.datetime.now()
+    self.expire_time = self.creation_time + expire_delta
+
+  def is_expired(self):
+    return datetime.datetime.now() > self.expire_time 
+
+def get_articles(context, scraper, period):
+  cache_key = f'{scraper}_{period}'
+  scraper_cache = context.bot_data.get('scraper_cache', dict())
+  scraper_result_cache = scraper_cache.get(cache_key)
+  if scraper_result_cache is None or scraper_result_cache.is_expired():
+    logger.info(f'Updating cache for {cache_key!r}')
+    scraper_result = scraper(error='ignore').fetch_and_parse(period)
+    scraper_result_cache = ScraperMemCache(
+      items=scraper_result,
+      cache_key=cache_key,
+      expire_delta=datetime.timedelta(seconds=30)
+    )
+    scraper_cache[cache_key] = scraper_result_cache
+  else:
+    logger.info(f'Get {cache_key!r} from cache')
+  scraper_result = scraper_result_cache.items
+  return scraper_result
 
 def cfa_news_factory(scraper, period, *, chat_id_from_update=True):
   async def cfa_news(context, effective_chat_id):
