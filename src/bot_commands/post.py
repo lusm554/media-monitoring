11ef1ad228f33f71a88c2471baf7c8e23a6eebbc
lusm <loveyousomuch554@gmail.com>
2024-05-02 19:30:38 +0300

    fix error with same post id due to place uuid4() in arguments default parameter

--- src/bot_commands/post.py
+++ src/bot_commands/post.py
@@ -2,10 +2,10 @@ import datetime
 from uuid import uuid4
 
 class Post:
-  def __init__(self, post_items, post_id=uuid4(), creation_time=None, page_items_cnt=4):
+  def __init__(self, post_items, post_id=None, creation_time=None, page_items_cnt=4):
     self._items_count_on_page = page_items_cnt
     self._current_page = 0
-    self.post_id = str(post_id)
+    self.post_id = post_id or str(uuid4())
     self.post_items = post_items
     self.pages = [
       self.post_items[i:i+self._items_count_on_page]
