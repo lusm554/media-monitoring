b68689edfe5009ed6fb06274257183b0c6b67b72
lusm <loveyousomuch554@gmail.com>
2024-05-03 20:09:29 +0300

    remove notes

--- src/main.py
+++ src/main.py
@@ -1,37 +1,3 @@
-'''
-Commands:
-  Standard:
-    - help
-    - start
-  Specific:
-    - last_news
-    - emits
-    - media_index
-    - media_blacklist
-    - set_news_schedule
-    - unset_news_schedule
-
-Functionality:
-  Main:
-    - cfa news
-    - cfa releases
-    - regular cfa news and releases
-  Side:
-    - show news blacklist
-    - show rss media index
-  
-Structure:
-  commands/
-  scraper/
-  main.py
-  ...other.py
-
-Handlers:
-  1. Command
-  2. buttons
-  3. Updates
-'''
-
 from timezone import time_zone_moscow
 import datetime
 import logging
