467eeb6af21e3885907b13866ce51f8028553bc2
lusm <loveyousomuch554@gmail.com>
2024-05-07 21:38:42 +0300

    call function

--- examples/pdf_parse.py
+++ examples/pdf_parse.py
@@ -5,6 +5,9 @@ filepath = 'pdfs/a-token_ab.pdf'
 
 def parse_pdf(filepath):
   with fitz.open(filepath) as document:
+    print(f'{document.page_count=}')
     pprint(document.metadata)
-
+    for page in document:
+      text = page.get_text()
+      print(text)
 parse_pdf(filepath)
