50d6f04a93329b8ba35ecc8f1cc51234fbc2c855
lusm <loveyousomuch554@gmail.com>
2024-05-17 14:15:02 +0300

    changed structure code

--- examples/pdf_parse.py
+++ examples/pdf_parse.py
@@ -1,13 +1,24 @@
 import fitz
+import re
 from pprint import pprint
 
 filepath = 'pdfs/a-token_ab.pdf'
 
-def parse_pdf(filepath):
+def pdf_to_text(filepath):
   with fitz.open(filepath) as document:
     print(f'{document.page_count=}')
     pprint(document.metadata)
+    document_text = ''
     for page in document:
       text = page.get_text()
-      print(text)
+      document_text += text
+    return document_text
+
+def parse_text(text):
+  pass
+
+def parse_pdf(filepath):
+  file_text = pdf_to_text(filepath)
+  res = parse_text(file_text)
+
 parse_pdf(filepath)
