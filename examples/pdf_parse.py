3865f26238ee49e29eb8c7940834e3fae4206148
lusm <loveyousomuch554@gmail.com>
2024-05-17 16:49:31 +0300

    add parse a-token text func

--- examples/pdf_parse.py
+++ examples/pdf_parse.py
@@ -19,21 +19,21 @@ def find_with_pattern(pattern, text):
     return match.group(1)
   return None
 
-def parse_text(text):
+def parse_a_token_text(text):
   cfa_nominal_pattern = re.compile(r'Цена приобретения ЦФА при их выпуске.*?составляет\s+(\d{1,3}(?: \d{3})*(?:,\d{2})?)', re.DOTALL)
   cfa_start_placement_dt_pattern = re.compile(r'Дата начала размещения ЦФА: (.*)')
   cfa_end_placement_dt_pattern = re.compile(r'Дата завершения \(окончания\) размещения ЦФА: (.*)')
   cfa_nominal_value = find_with_pattern(cfa_nominal_pattern, text)
   cfa_start_placement_dt = find_with_pattern(cfa_start_placement_dt_pattern, text)
   cfa_end_placement_dt = find_with_pattern(cfa_end_placement_dt_pattern, text)
+  return cfa_end_placement_dt, cfa_start_placement_dt, cfa_nominal_value
+
+def parse_pdf(filepath, parse_text):
+  file_text = pdf_to_text(filepath)
+  cfa_end_placement_dt, cfa_start_placement_dt, cfa_nominal_value = parse_text(file_text)
   print(cfa_nominal_value)
   print(cfa_start_placement_dt)
   print(cfa_end_placement_dt)
-  assert not None in (cfa_end_placement_dt, cfa_start_placement_dt, cfa_nominal_value)
-
-def parse_pdf(filepath):
-  file_text = pdf_to_text(filepath)
-  res = parse_text(file_text)
 
 def main():
   '''
@@ -43,9 +43,10 @@ def main():
   for root, dirs, files in os.walk('pdfs/'):
     for file in files:
       if not file.startswith('a-token'): continue
+      #if not file.startswith('sberbank'): continue
       filepath = os.path.join(root, file)
       print(filepath)
-      parse_pdf(filepath)
+      parse_pdf(filepath, parse_a_token_text)
       print()
 
 if __name__ == '__main__':
