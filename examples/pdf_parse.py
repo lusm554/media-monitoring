1db2dfb8c38fc6f015521f64250e4779a5015c50
lusm <loveyousomuch554@gmail.com>
2024-05-17 16:36:08 +0300

    parse end placement dt for a-token platform

--- examples/pdf_parse.py
+++ examples/pdf_parse.py
@@ -22,10 +22,14 @@ def find_with_pattern(pattern, text):
 def parse_text(text):
   cfa_nominal_pattern = re.compile(r'Цена приобретения ЦФА при их выпуске.*?составляет\s+(\d{1,3}(?: \d{3})*(?:,\d{2})?)', re.DOTALL)
   cfa_start_placement_dt_pattern = re.compile(r'Дата начала размещения ЦФА: (.*)')
+  cfa_end_placement_dt_pattern = re.compile(r'Дата завершения \(окончания\) размещения ЦФА: (.*)')
   cfa_nominal_value = find_with_pattern(cfa_nominal_pattern, text)
   cfa_start_placement_dt = find_with_pattern(cfa_start_placement_dt_pattern, text)
+  cfa_end_placement_dt = find_with_pattern(cfa_end_placement_dt_pattern, text)
   print(cfa_nominal_value)
   print(cfa_start_placement_dt)
+  print(cfa_end_placement_dt)
+  assert not None in (cfa_end_placement_dt, cfa_start_placement_dt, cfa_nominal_value)
 
 def parse_pdf(filepath):
   file_text = pdf_to_text(filepath)
