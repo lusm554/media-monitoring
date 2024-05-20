778f3dc6d0810b199da524db45746462e974cdbd
lusm <loveyousomuch554@gmail.com>
2024-05-20 12:36:03 +0300

    try to parse pdf through nltk, file scheme

---
+++ examples/nlkt_pdfs_parse.py
@@ -0,0 +1,42 @@
+import fitz
+import re
+import os
+from pprint import pprint
+
+def pdf_to_text(filepath):
+  with fitz.open(filepath) as document:
+    #print(f'{document.page_count=}')
+    #pprint(document.metadata)
+    document_text = ''
+    for page in document:
+      text = page.get_text()
+      document_text += text
+  document_text = document_text.replace('\n', ' ')
+  document_text = re.sub(" +", " ", document_text)
+  document_text = document_text.strip()
+  return document_text
+
+
+def main():
+  filepath = 'pdfs/a-token_alrosa.pdf'
+  '''
+  for root, dirs, files in os.walk('pdfs/'):
+    for file in files:
+      filepath = os.path.join(root, file)
+      if file.startswith('a-token'): 
+        print(filepath)
+        parse_pdf(filepath, atoken_patterns)
+      if file.startswith('sberbank'):
+        continue
+        print(filepath)
+        parse_pdf(filepath, sberbank_patterns)
+        pass
+      if file.startswith('tokeon'):
+        continue
+        print(filepath)
+        parse_pdf(filepath, tokeon_patterns)
+        pass
+  '''
+
+if __name__ == '__main__':
+  main()
