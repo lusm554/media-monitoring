9e0f4a5f3002de00e65a22a2e3d71c54e91a1064
lusm <loveyousomuch554@gmail.com>
2024-05-20 12:36:58 +0300

    remove print metadata and text preprocssing

--- examples/nlkt_pdfs_parse.py
+++ examples/nlkt_pdfs_parse.py
@@ -5,20 +5,20 @@ from pprint import pprint
 
 def pdf_to_text(filepath):
   with fitz.open(filepath) as document:
-    #print(f'{document.page_count=}')
-    #pprint(document.metadata)
     document_text = ''
     for page in document:
       text = page.get_text()
       document_text += text
-  document_text = document_text.replace('\n', ' ')
-  document_text = re.sub(" +", " ", document_text)
-  document_text = document_text.strip()
+  #document_text = document_text.replace('\n', ' ')
+  #document_text = re.sub(" +", " ", document_text)
+  #document_text = document_text.strip()
   return document_text
 
 
 def main():
   filepath = 'pdfs/a-token_alrosa.pdf'
+  filetext = pdf_to_text(filepath)
+  print(filetext)
   '''
   for root, dirs, files in os.walk('pdfs/'):
     for file in files:
