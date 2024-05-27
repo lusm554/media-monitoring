933b3a96b86b65e403badb020e48daacc2d2b279
lusm <loveyousomuch554@gmail.com>
2024-05-27 15:29:51 +0300

    plan

--- examples/natasha_fragment_parse_exampe.py
+++ examples/natasha_fragment_parse_exampe.py
@@ -16,21 +16,21 @@ addr_extractor = AddrExtractor(morph_vocab)
 '''
 Parse plan:
   Parse pdf to text:
-    ...
+    1. Through fitz parse pdf pages to text string
   Parse text:
-    ...
+    1. Segment text - split on tokens and sentences
+    2. Tag morph and parse syntax
+    3. Tag ner
+    4. Lemmatization
+    5. Normalization
+    6. Get period text fragment
+    7. Get nominal text fragment
     Parse start, end dt:
-      ...
+      1. Parse all dates in text fragment
+      2. By keywords detect start and end placement date
     Parse nominal:
-      ...
-
-  1. Segment text - split on tokens and sentences
-  2. Tag morph and parse syntax
-  3. Tag ner
-  4. Lemmatization 
-  5. Normalization
-  6. Check dt context key words
-  7. Return result
+      1. Parse all money in text frament
+      2. By keywords detect cfa nominal
 '''
 
 '''
