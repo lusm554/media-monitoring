677f8a7c3a8e49e949e20efe091bad61172555bf
lusm <loveyousomuch554@gmail.com>
2024-07-06 18:05:00 +0300

    rename to junk

--- r_and_d/_scraper_module/scraper/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base_scraper import Periods
-from .dzen_news_scraper import CfaDzenNewsScraper
-from .rss_news_scraper import CfaRssNewsScraper
-from .google_news_scraper import CfaGoogleNewsScraper
-from .all_news_scraper import CfaAllNewsScraper
-from .cfaru_releases_scraper import CfaReleasesScraper
