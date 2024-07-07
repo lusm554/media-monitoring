474f93ba89cf5c59f13bf177cf5c9220187ebb39
lusm <loveyousomuch554@gmail.com>
2024-07-07 14:28:15 +0300

    add logging & cleanup code

--- bot/bot.py
+++ bot/bot.py
@@ -5,22 +5,36 @@ from storage import insert_row
 import scraper
 from bot.setup import (
   setup_bot_data_variables,
-  setup_bot_handlers,
+  setup_basic_handlers,
   setup_bot_commands,
   shedule_bot_tasks,
   update_commands_ui_description,
 )
-#from bot.handlers import unknown_command_handler, error_handler, updates_handler
+from bot.handlers import unknown_command_handler, error_handler, updates_handler
 import bot.commands as bot_commands
+
+import datetime
+import logging
+logging.basicConfig(
+  level=logging.INFO,
+  format='[%(asctime)s] %(levelname)s [%(name)s] %(message)s',
+  datefmt='%Y-%m-%d %H:%M:%S %Z',
+  handlers=[
+    #logging.FileHandler(datetime.datetime.now().strftime('shared/logs/log_%Y-%m-%d_%H-%M-%S.log')),
+    logging.StreamHandler(),
+  ],
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 
 def setup(telegram_app):
   Command = namedtuple('Cmd', ['callback', 'desc', 'name'])
   commands = (
     Command(callback=bot_commands.start, desc='начать работу', name='start'),
   )
-  setup_bot_commands(telegram_app, commands)
   #setup_bot_data_variables(telegram_app, commands)
-  #setup_bot_handlers(telegram_app, commands, unknown_command_handler, error_handler, updates_handler)
+  setup_bot_commands(telegram_app, commands)
+  setup_basic_handlers(telegram_app, error_handler, unknown_command_handler, updates_handler)
   #shedule_bot_tasks(telegram_app)
   update_commands_ui_description(telegram_app, commands)
 
--- bot/setup.py
+++ bot/setup.py
@@ -12,18 +12,7 @@ from telegram.ext import (
 import asyncio
 
 def setup_bot_data_variables(telegram_app, commands):
-  '''
-  telegram_app.bot_data['commands'] = commands
-  telegram_app.bot_data['scraper'] = scraper
-  telegram_app.bot_data['post_cache'] = dict()
-  telegram_app.bot_data['scraper_cache'] = dict()
-  #telegram_app.bot_data['regular_newsletter_chats'] = set(str(x) for x in os.environ.get('NEWS_SCHEDULED_CHATS').split(',') if x != '')
-  telegram_app.bot_data['bot_timezone'] = time_zone_moscow
-  telegram_app.bot_data['cfa_newsletter_time'] = datetime.time(hour=9, tzinfo=time_zone_moscow)
-  telegram_app.bot_data['cfa_releases_time'] = datetime.time(hour=19, tzinfo=time_zone_moscow)
-  telegram_app.bot_data['DEVELOPER_CHAT_ID'] = os.environ.get('DEVELOPER_CHAT_ID')
-  telegram_app.bot_data['commands'] = commands
-  '''
+	pass
 
 def setup_bot_commands(telegram_app, commands):
   for command in commands:
@@ -34,54 +23,10 @@ def setup_bot_commands(telegram_app, commands):
       )
     )
 
-def setup_bot_handlers(telegram_app, commands, unknown_command_handler, error_handler, updates_handler):
+def setup_basic_handlers(telegram_app, error_handler, unknown_command_handler, updates_handler):
   telegram_app.add_handler(TypeHandler(Update, updates_handler), -1)
-  for command in commands:
-    telegram_app.add_handler(
-      CommandHandler(
-        command=command.name,
-        callback=command.callback
-      )
-    )
-  '''
-  telegram_app.add_handler(
-    CallbackQueryHandler(
-      bot_handlers.button_dispatcher({
-        'cfa_last_news': bot_commands.cfa_last_news_button_callback,
-        'cfa_releases': bot_commands.cfa_releases_button_callback,
-      })
-    )
-  )
-  '''
   telegram_app.add_handler(MessageHandler(filters.COMMAND, unknown_command_handler))
   telegram_app.add_error_handler(error_handler)
-
-def shedule_bot_tasks(telegram_app):
-  job_interval = datetime.timedelta(hours=1)
-  job_time_to_first_run = datetime.timedelta(seconds=30)
-  newsletter_time = telegram_app.bot_data.get('cfa_newsletter_time')
-  releases_time = telegram_app.bot_data.get('cfa_releases_time')
-  telegram_app.job_queue.run_repeating(
-    callback=bot_regular_tasks.post_cache_cleaner,
-    interval=job_interval,
-    first=job_time_to_first_run,
-  )
-  telegram_app.job_queue.run_daily(
-    callback=bot_regular_tasks.cfa_news_sender,
-    time=newsletter_time,
-    data=bot_commands.cfa_last_news_regular,
-  )
-  telegram_app.job_queue.run_daily(
-    callback=bot_regular_tasks.cfa_releases_sender,
-    time=releases_time,
-    data=bot_commands.cfa_last_releases_regular,
-  )
-  # telegram_app.job_queue.run_repeating(
-  #   bot_regular_tasks.cfa_news_sender,
-  #   interval=60,
-  #   first=15,
-  #   data=bot_commands.cfa_last_releases_regular,
-  # )
 
 def update_commands_ui_description(telegram_app, commands):
   cmds = [
@@ -90,3 +35,9 @@ def update_commands_ui_description(telegram_app, commands):
   ]
   loop = asyncio.get_event_loop()
   loop.run_until_complete(telegram_app.bot.set_my_commands(cmds))
+
+def setup_button_dispatcher_handlers():
+	pass
+
+def shedule_bot_tasks(telegram_app):
+	pass
