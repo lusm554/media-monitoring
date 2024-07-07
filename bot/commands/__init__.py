bf68905a1161ea026a39e2248e1f6b7bfcafedca
lusm <loveyousomuch554@gmail.com>
2024-07-07 14:39:32 +0300

    move command start from init to start.py

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,11 +1,1 @@
-async def start(update, context):
-  msg = (
-    f'Привет!\n'
-    f'Этот бот собирает публикации СМИ и выпуски по ЦФА.\n'
-    f'Рассылка новостей /subscribe_news.\n'
-    f'Более подробно через команду /help.'
-  )
-  await context.bot.send_message(
-    chat_id=update.effective_chat.id,
-    text=msg
-  )
+from bot.commands.start import start
