70ca0e47de478d98e048521b5fdd1afe398144af
lusm <loveyousomuch554@gmail.com>
2024-08-29 12:29:06 +0300

    regular news by subscribers list from db

--- bot/regular_tasks/cfa_news_sender.py
+++ bot/regular_tasks/cfa_news_sender.py
@@ -1,6 +1,9 @@
 from bot.commands import cfa_news
+import storage
 
 async def cfa_news_sender(context):
   # iter over news chats & send news
-  target_chat_id = '421489738'
-  await cfa_news(context, target_chat_id)
+  #target_chat_id = '421489738'
+  for subscriber in storage.get_n_news_subscribers(n=1000): # bug?
+    target_chat_id = subscriber['telegram_user_id']
+    await cfa_news(context, target_chat_id)
