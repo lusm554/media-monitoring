d61f748d17ddf5b1cf551e9ffd1d4937f56c2b15
lusm <loveyousomuch554@gmail.com>
2024-09-17 15:29:50 +0300

    removed junk nlp

--- nlp/parsepdf.py
+++
@@ -1,8 +0,0 @@
-def pdf_to_text(pdf_filepath):
-	from pdfminer.high_level import extract_text
-	import re
-	text = extract_text("cfa.pdf")
-	text = re.sub(r'\s+', ' ', text) 
-	text = re.sub(r'[^\w\s.,!?-]', '', text)
-	print(len(text))
-	return text
