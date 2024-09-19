fc6fc4c8bc02d08f79e51c775d1cbdbcda2d6294
lusm <loveyousomuch554@gmail.com>
2024-09-19 22:12:54 +0300

    news scrapper every 15 minutes

--- bot/setup.py
+++ bot/setup.py
@@ -46,7 +46,7 @@ def setup_button_handlers(telegram_app, cfa_last_news_button_callback, cfa_last_
   telegram_app.add_handler(CallbackQueryHandler(cfa_last_releases_button_callback, pattern='cfa_last_releases*'))
 
 def shedule_regular_bot_tasks(telegram_app):
-  scraper_news_job_interval = datetime.timedelta(minutes=10)
+  scraper_news_job_interval = datetime.timedelta(minutes=15)
   scraper_releases_job_interval = datetime.timedelta(minutes=60)
   regular_news_time = datetime.time(hour=9, tzinfo=zoneinfo.ZoneInfo("Europe/Moscow"))
   regular_releases_time = datetime.time(hour=19, tzinfo=zoneinfo.ZoneInfo("Europe/Moscow"))
